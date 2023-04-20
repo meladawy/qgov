59b4055a77607c1a2d5da099134471864f63b774
Maged Eladawy <magedbik@gmail.com>
2023-04-21 00:11:01 +0200

    Change the execution event

--- src/js/main.js
+++ src/js/main.js
@@ -1,77 +1,70 @@
-// Add custom login when window loaded using javascript and without using jQuery.
-window.onload = function () {
+document.addEventListener('DOMContentLoaded', function () {
+  // Add custom login when window loaded using javascript and without using jQuery.
   const megaMenu = `
-    <nav class="mega-menu">
-        <div class="menu-container">
-            <div class="logo">
-                <a href="#"><img src="images/logo.png" alt="Your Logo"></a>
-            </div>
-            <ul class="menu-items">
-                <li class="submenu-parent">
-                    <a href="#">About</a>
-                    <div class="submenu">
-                        <div class="submenu-links-wrapper">
-                            <div class="submenu-section">
-                                <a href="/about">What is Quantgov</a>
-                            </div>
-                            <div class="submenu-section">
-                                <a href="/our-work-in-action">Our work in action</a>
-                            </div>
-                        </div>
-                    </div>
-                </li>
+  <nav class="mega-menu">
+      <div class="menu-container">
+          <ul class="menu-items">
+              <li class="submenu-parent">
+                  <a href="#">About</a>
+                  <div class="submenu">
+                      <div class="submenu-links-wrapper">
+                          <div class="submenu-section">
+                              <a href="/about">What is Quantgov</a>
+                          </div>
+                          <div class="submenu-section">
+                              <a href="/our-work-in-action">Our work in action</a>
+                          </div>
+                      </div>
+                  </div>
+              </li>
 
-                <li class="submenu-parent">
-                    <a href="#">Data</a>
-                    <div class="submenu">
-                        <div class="submenu-links-wrapper">
-                            <div class="submenu-section">
-                                <a href="https://www.reghub.ai/data/bulk">CSVs</a>
-                            </div>
-                            <div class="submenu-section">
-                                <a href="https://www.reghub.ai/data/custom">Interactive Downloader</a>
-                            </div>
-                            <div class="submenu-section" data-extra-details="submenu-section-apis">
-                                <h3>APIs <i class="fas fa-chevron-right"></i></h3>
-                                <ul>
-                                    <li><a href="#">Subcategory 2.1</a></li>
-                                    <li><a href="#">Subcategory 2.2</a></li>
-                                    <li><a href="#">Subcategory 2.3</a></li>
-                                </ul>
-                            </div>
-                        </div>
+              <li class="submenu-parent">
+                  <a href="#">Data</a>
+                  <div class="submenu">
+                      <div class="submenu-links-wrapper">
+                          <div class="submenu-section">
+                              <a href="https://www.reghub.ai/data/bulk">CSVs</a>
+                          </div>
+                          <div class="submenu-section">
+                              <a href="https://www.reghub.ai/data/custom">Interactive Downloader</a>
+                          </div>
+                          <div class="submenu-section" data-extra-details="submenu-section-apis">
+                              <h3>APIs <i class="fas fa-chevron-right"></i></h3>
+                              <ul>
+                                  <li><a href="#">Subcategory 2.1</a></li>
+                                  <li><a href="#">Subcategory 2.2</a></li>
+                                  <li><a href="#">Subcategory 2.3</a></li>
+                              </ul>
+                          </div>
+                      </div>
 
-                        <div class="submenu-extra-content-wrapper">
-                            <div class="submenu-extra-section-item" id="submenu-section-apis">
-                                <h3>Our APIs</h3>
-                                <ul>
-                                    <li><a href="https://api.quantgov.org/swagger-ui.html">RegCensus API</a></li>
-                                    <li><a href="https://github.com/QuantGov/regcensus-api-python">Python API Package</a></li>
-                                    <li><a href="https://github.com/QuantGov/regcensus-api-client-R">R API Package</a></li>
-                                    <li><a href="https://www.quantgov.org/api-python-library-full-tutorial">API Help Guide</a></li>
-                                    <li><a href="https://new-website-files.s3.amazonaws.com/Regcensus+API+Patch+2.1.pdf">Patch Notes</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <!-- Add more submenu-sections as needed -->
-                    </div>
-                </li>
-                <li><a href="#">Visuals</a></li>
-                <li><a href="#">State Snapshots</a></li>
-                <li><a href="#">AI-Readable Laws & Rules</a></li>
-                <li><a href="#">Get Involved</a></li>
-            </ul>
-            <div class="search">
-                <input type="search" placeholder="Search...">
-            </div>
-        </div>
-    </nav>
-  `;
+                      <div class="submenu-extra-content-wrapper">
+                          <div class="submenu-extra-section-item" id="submenu-section-apis">
+                              <h3>Our APIs</h3>
+                              <ul>
+                                  <li><a href="https://api.quantgov.org/swagger-ui.html">RegCensus API</a></li>
+                                  <li><a href="https://github.com/QuantGov/regcensus-api-python">Python API Package</a></li>
+                                  <li><a href="https://github.com/QuantGov/regcensus-api-client-R">R API Package</a></li>
+                                  <li><a href="https://www.quantgov.org/api-python-library-full-tutorial">API Help Guide</a></li>
+                                  <li><a href="https://new-website-files.s3.amazonaws.com/Regcensus+API+Patch+2.1.pdf">Patch Notes</a></li>
+                              </ul>
+                          </div>
+                      </div>
+                      <!-- Add more submenu-sections as needed -->
+                  </div>
+              </li>
+              <li><a href="#">Visuals</a></li>
+              <li><a href="#">State Snapshots</a></li>
+              <li><a href="#">AI-Readable Laws & Rules</a></li>
+              <li><a href="#">Get Involved</a></li>
+          </ul>
+      </div>
+  </nav>
+`;
 
   // If element with class '.header-nav' exists, replace it with megaMenu.
   if (document.querySelector('.header-nav')) {
     document.querySelector('.header-nav').innerHTML = megaMenu;
-
     // Show .header-nav element after replacing it with megaMenu.
     document.querySelector('.header-nav').style.display = 'block';
   }
@@ -137,4 +130,5 @@ window.onload = function () {
       }
     });
   }
-}
+
+});
